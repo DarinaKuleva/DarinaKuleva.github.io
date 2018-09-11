15b724d774edefb2aea4eab0604f1ed5dbc8c93c
Darina Kuleva <darinakuleva@gmail.com>
2018-09-11 16:25:40 +0300

    верстка about

--- gulpfile.js
+++ gulpfile.js
@@ -3,11 +3,11 @@ var sass = require('gulp-sass');
 
 
 gulp.task('sass', function(){
-  gulp.src('source/sass/style.scss')
+  gulp.src('sass/style.scss')
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('source/css'));
+    .pipe(gulp.dest('css'));
 });
 
 gulp.task('sass:watch', function(){
-  gulp.watch('source/sass/**/*.{scss,sass}', ['style']);
+  gulp.watch('sass/**/*.{scss,sass}', ['style']);
   })
